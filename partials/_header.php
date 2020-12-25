cc504be9dd2fd92ac48570165c4fa500a78dd8c6
noobcoding23 <jugalkdas99@gmail.com>
2020-12-25 19:01:35 +0530

    added login/signup, dbconnect and modal

--- partials/_header.php
+++ partials/_header.php
@@ -1,6 +1,6 @@
 <?php
 echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-<a class="navbar-brand" href="#">JakeForum</a>
+<a class="navbar-brand" href="/">JakeForum</a>
 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
 </button>
@@ -8,10 +8,10 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
 <div class="collapse navbar-collapse" id="navbarSupportedContent">
   <ul class="navbar-nav mr-auto">
     <li class="nav-item active">
-      <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#">About</a>
+      <a class="nav-link" href="/about.php">About</a>
     </li>
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
@@ -25,7 +25,7 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       </div>
     </li>
     <li class="nav-item">
-      <a class="nav-link" href="#" tabindex="-1">Contact</a>
+      <a class="nav-link" href="/contact.php" tabindex="-1">Contact</a>
     </li>
   </ul>
   <div class="row mx-2">
@@ -33,10 +33,13 @@ echo '<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-success my-2 my-sm-0" type="submit">Search</button>
     </form>
-    <div class="button btn btn-outline-success ml-2">Login</div>
-    <div class="button btn btn-outline-success mx-2">SignUp</div>
+    <div class="button btn btn-outline-success ml-2" data-toggle="modal" data-target="#loginModal">Login</div>
+    <div class="button btn btn-outline-success mx-2" data-toggle="modal" data-target="#signupModal">SignUp</div>
   </div>
 </div>
 </nav>';
+
+include '_loginModal.php';
+include '_signupModal.php';
 
 ?>
