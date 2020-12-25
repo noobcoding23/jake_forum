86729f3aa3648deedd6f5c6c0e9cefe008ca1897
noobcoding23 <jugalkdas99@gmail.com>
2020-12-25 21:17:23 +0530

    more files

--- index.php
+++ index.php
@@ -26,16 +26,13 @@
         </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
-                <img src="https://source.unsplash.com/2400x700/?programming,coding
-          " class="d-block w-100" alt="..." />
+                <img src="/img/slider-1.jpeg" class="d-block w-100" alt="..." />
             </div>
             <div class="carousel-item">
-                <img src="https://source.unsplash.com/2400x700/?php,javascript
-          " class="d-block w-100" alt="..." />
+                <img src="/img/slider-2.jpeg" class="d-block w-100" alt="..." />
             </div>
             <div class="carousel-item">
-                <img src="https://source.unsplash.com/2400x700/?microsoft,apple
-          " class="d-block w-100" alt="..." />
+                <img src="/img/slider-3.jpeg" class="d-block w-100" alt="..." />
             </div>
         </div>
         <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
---
+++ threads.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous" />
+
+    <title>Welcome to JakeForum - Coding forum</title>
+</head>
+
+<body>
+    <?php include 'partials/_header.php'?>
+    <?php include 'partials/_dbconnect.php'?>
+
+    <div class="container my-4">
+        <div class="jumbotron">
+            <h1 class="display-4">Welcome to Python forums</h1>
+            <p class="lead">React is an open-source, front end, JavaScript library for building user interfaces or UI
+                components.</p>
+            <hr class="my-4">
+            <p>This is a peer to peer forum is for sharing knowledge with each other. No Spam / Advertising / Self-promote in the forums is not allowed. Do not post copyright-infringing material.
+                Do not post “offensive” posts, links or images.
+                Remain respectful of other members at all times.</p>
+            <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+        </div>
+    </div>
+
+    <div class="container">
+        <h1 class="py-2">Browse Quetions</h1>
+        <div class="media my-3">
+            <img src="/img/userdefault.png" width="54px" class="mr-3" alt="...">
+            <div class="media-body">
+                <h5 class="mt-0">Unable to install Pyaudio error in Windows</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus
+                odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+        </div>
+
+        <div class="media my-3">
+            <img src="/img/userdefault.png" width="54px" class="mr-3" alt="...">
+            <div class="media-body">
+                <h5 class="mt-0">Unable to install Pyaudio error in Windows</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus
+                odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+        </div>
+
+        <div class="media my-3">
+            <img src="/img/userdefault.png" width="54px" class="mr-3" alt="...">
+            <div class="media-body">
+                <h5 class="mt-0">Unable to install Pyaudio error in Windows</h5>
+                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus
+                odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate
+                fringilla. Donec lacinia congue felis in faucibus.
+            </div>
+        </div>
+    </div>
+
+    <?php include 'partials/_footer.php'?>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
